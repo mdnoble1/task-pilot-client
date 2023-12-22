b8cf1d1bad7ef2ded366a8749ee7234a2cc3d801
Md Noble <mdnoble.wd@gmail.com>
2023-12-22 10:31:49 +0600

    installed tailwind css and daisy ui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>Task Pilot</h2>
+      <button className='btn btn-outline'>Button</button>
+    </>
+  )
+}
+
+export default App
