475ef321e89ab571a807eb32f66be64f9336368d
Md Noble <mdnoble.wd@gmail.com>
2023-12-22 17:52:54 +0600

    updated banner style

--- src/pages/home/banner/Banner.jsx
+++ src/pages/home/banner/Banner.jsx
@@ -3,8 +3,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-import img1 from "../../../assets/images/task1.png";
-import img2 from "../../../assets/images/task2.png";
+import img1 from "../../../assets/images/task2.png";
+import img2 from "../../../assets/images/task1.png";
 import img3 from "../../../assets/images/task3.png";
 import img4 from "../../../assets/images/task4.png";
 import img5 from "../../../assets/images/task5.png";
@@ -14,7 +14,7 @@ import { FaRegPaperPlane } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <section>
+    <section className="lg:mt-1">
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
@@ -30,10 +30,14 @@ const Banner = () => {
         className="mySwiper"
       >
         <SwiperSlide className="relative">
-          <img className="h-full lg:h-[700px] w-full" src={img1} alt="" />
-          <div className="absolute bottom-[20px] left-[150px] md:bottom-[60px] md:left-[300px] lg:bottom-[120px] lg:left-[675px]">
+          <img
+            className="h-full lg:h-[700px] w-full opacity-90 lg:rounded-md"
+            src={img1}
+            alt=""
+          />
+          <div className="absolute bottom-[40px] left-[150px] md:bottom-[60px] md:left-[290px] lg:bottom-[140px] lg:left-[650px]">
             <NavLink to="/login">
-              <button className="btn btn-sm md:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase">
+              <button className="btn btn-sm md:btn-md lg:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase drop-shadow-2xl">
                 Let's Explore{" "}
                 <FaRegPaperPlane className="text-lg md:text-2xl"></FaRegPaperPlane>
               </button>
@@ -41,10 +45,14 @@ const Banner = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className="relative">
-          <img className="h-full lg:h-[700px] w-full" src={img2} alt="" />
-          <div className="absolute bottom-[20px] left-[150px] md:bottom-[60px] md:left-[300px] lg:bottom-[120px] lg:left-[675px]">
+          <img
+            className="h-full lg:h-[700px] w-full opacity-90 lg:rounded-md"
+            src={img2}
+            alt=""
+          />
+          <div className="absolute bottom-[40px] left-[150px] md:bottom-[60px] md:left-[290px] lg:bottom-[140px] lg:left-[650px]">
             <NavLink to="/login">
-              <button className="btn btn-sm md:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase">
+              <button className="btn btn-sm md:btn-md lg:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase drop-shadow-2xl">
                 Let's Explore{" "}
                 <FaRegPaperPlane className="text-lg md:text-2xl"></FaRegPaperPlane>
               </button>
@@ -52,10 +60,14 @@ const Banner = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className="relative">
-          <img className="h-full lg:h-[700px] w-full" src={img3} alt="" />
-          <div className="absolute bottom-[20px] left-[150px] md:bottom-[60px] md:left-[300px] lg:bottom-[120px] lg:left-[675px]">
+          <img
+            className="h-full lg:h-[700px] w-full opacity-90 lg:rounded-md"
+            src={img3}
+            alt=""
+          />
+          <div className="absolute bottom-[40px] left-[150px] md:bottom-[60px] md:left-[290px] lg:bottom-[140px] lg:left-[650px]">
             <NavLink to="/login">
-              <button className="btn btn-sm md:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase">
+              <button className="btn btn-sm md:btn-md lg:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase drop-shadow-2xl">
                 Let's Explore{" "}
                 <FaRegPaperPlane className="text-lg md:text-2xl"></FaRegPaperPlane>
               </button>
@@ -63,10 +75,14 @@ const Banner = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className="relative">
-          <img className="h-full lg:h-[700px] w-full" src={img4} alt="" />
-          <div className="absolute bottom-[20px] left-[150px] md:bottom-[60px] md:left-[300px] lg:bottom-[120px] lg:left-[675px]">
+          <img
+            className="h-full lg:h-[700px] w-full opacity-90 lg:rounded-md"
+            src={img4}
+            alt=""
+          />
+          <div className="absolute bottom-[40px] left-[150px] md:bottom-[60px] md:left-[290px] lg:bottom-[140px] lg:left-[650px]">
             <NavLink to="/login">
-              <button className="btn btn-sm md:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase">
+              <button className="btn btn-sm md:btn-md lg:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase drop-shadow-2xl">
                 Let's Explore{" "}
                 <FaRegPaperPlane className="text-lg md:text-2xl"></FaRegPaperPlane>
               </button>
@@ -74,10 +90,14 @@ const Banner = () => {
           </div>
         </SwiperSlide>
         <SwiperSlide className="relative">
-          <img className="h-full lg:h-[700px] w-full" src={img5} alt="" />
-          <div className="absolute bottom-[20px] left-[150px] md:bottom-[60px] md:left-[300px] lg:bottom-[120px] lg:left-[675px]">
+          <img
+            className="h-full lg:h-[700px] w-full opacity-90 lg:rounded-md"
+            src={img5}
+            alt=""
+          />
+          <div className="absolute bottom-[40px] left-[150px] md:bottom-[60px] md:left-[290px] lg:bottom-[140px] lg:left-[650px]">
             <NavLink to="/login">
-              <button className="btn btn-sm md:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase">
+              <button className="btn btn-sm md:btn-md lg:btn-lg bg-[#129efc] hover:bg-blue-700 border-0 font-semibold md:text-2xl text-white uppercase drop-shadow-2xl">
                 Let's Explore{" "}
                 <FaRegPaperPlane className="text-lg md:text-2xl"></FaRegPaperPlane>
               </button>
