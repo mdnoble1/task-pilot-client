7e51e9c0dec5a97c0dc7fa65dc6869f59eb83e1d
Md Noble <mdnoble.wd@gmail.com>
2024-01-06 23:43:41 +0600

    added styles and media queries in taskSpace page

--- src/pages/dashboard/TaskSpace/TaskSpace.jsx
+++ src/pages/dashboard/TaskSpace/TaskSpace.jsx
@@ -14,38 +14,46 @@ const TaskSpace = () => {
 
   return (
     <section>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 w-11/12 mx-auto my-16">
-        {/* to do  */}
-        <div className="border-2 border-gray-700 rounded-t-lg h-[800px] overflow-y-scroll">
-          <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mt-6">
-            To Do
-          </h2>
-          <div>
-            {todoTasks?.map((task) => (
-              <TaskSpaceCard1 key={task._id} task={task}></TaskSpaceCard1>
-            ))}
+      <div className="hidden lg:block">
+        <div className="grid grid-cols-3 w-11/12 pt-6 hover:pt-0 px-16 hover:px-0 opacity-60 hover:opacity-100 duration-700 ease-in-out mx-auto mt-4 lg:mt-10">
+          {/* to do  */}
+          <div className="scale-50 hover:scale-100 duration-700 ease-in-out">
+            <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mb-6">
+              To Do
+            </h2>
+            <div className="border-2 border-gray-700 rounded-t-xl h-[390px] lg:h-[800px] overflow-y-scroll ">
+              <div>
+                {todoTasks?.map((task) => (
+                  <TaskSpaceCard1 key={task._id} task={task}></TaskSpaceCard1>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-        {/* on going  */}
-        <div className="border-2 border-gray-700 rounded-t-lg h-[800px] overflow-y-scroll">
-          <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mt-6">
-            On GOing
-          </h2>
-          <div>
-            {ongoingTasks?.map((task) => (
-              <TaskSpaceCard2 key={task._id} task={task}></TaskSpaceCard2>
-            ))}
+          {/* on going  */}
+          <div className="scale-50 hover:scale-100 duration-700 ease-in-out">
+            <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mb-6">
+              On GOing
+            </h2>
+            <div className="border-2 border-gray-700 rounded-t-xl h-[390px] lg:h-[800px] overflow-y-scroll ">
+              <div>
+                {ongoingTasks?.map((task) => (
+                  <TaskSpaceCard2 key={task._id} task={task}></TaskSpaceCard2>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-        {/* completed  */}
-        <div className="border-2 border-gray-700 rounded-t-lg h-[800px] overflow-y-scroll">
-          <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mt-6">
-            Completed
-          </h2>
-          <div>
-            {completedTasks?.map((task) => (
-              <TaskSpaceCard3 key={task._id} task={task}></TaskSpaceCard3>
-            ))}
+          {/* completed  */}
+          <div className="scale-50 hover:scale-100 duration-700 ease-in-out">
+            <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mb-6">
+              Completed
+            </h2>
+            <div className="border-2 border-gray-700 rounded-t-xl h-[390px] lg:h-[800px] overflow-y-scroll ">
+              <div>
+                {completedTasks?.map((task) => (
+                  <TaskSpaceCard3 key={task._id} task={task}></TaskSpaceCard3>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </div>
