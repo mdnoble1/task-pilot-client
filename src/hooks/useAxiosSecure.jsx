c3956fd84c9ef558ab98412cff75a971c4f7cdb6
Md Noble <mdnoble.wd@gmail.com>
2024-01-07 11:03:39 +0600

    updated styles and media queries in taskSpace page

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const axiosSecure = axios.create({
-    baseURL: "http://localhost:5000/"
-    // baseURL: "https://task-pilot-server-chi.vercel.app/"
+    // baseURL: "http://localhost:5000/"
+    baseURL: "https://task-pilot-server-chi.vercel.app/"
 }) 
 
 const useAxiosSecure = () => {
--- src/pages/dashboard/TaskSpace/TaskSpace.jsx
+++ src/pages/dashboard/TaskSpace/TaskSpace.jsx
@@ -15,13 +15,13 @@ const TaskSpace = () => {
   return (
     <section>
       <div className="hidden lg:block">
-        <div className="grid grid-cols-3 w-11/12 pt-6 hover:pt-0 px-16 hover:px-0 opacity-60 hover:opacity-100 duration-700 ease-in-out mx-auto mt-4 lg:mt-10">
+        <div className="grid grid-cols-3 w-11/12 pt-6 hover:pt-0 px-16 hover:px-0 opacity-50 hover:opacity-100 duration-700 ease-in-out mx-auto mt-4 lg:mt-10">
           {/* to do  */}
-          <div className="scale-50 hover:scale-100 duration-700 ease-in-out">
+          <div className="scale-75 hover:scale-100 duration-700 ease-in-out">
             <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mb-6">
               To Do
             </h2>
-            <div className="border-2 border-gray-700 rounded-t-xl h-[390px] lg:h-[800px] overflow-y-scroll ">
+            <div className="border-2 border-gray-500 h-[800px] overflow-y-scroll ">
               <div>
                 {todoTasks?.map((task) => (
                   <TaskSpaceCard1 key={task._id} task={task}></TaskSpaceCard1>
@@ -30,11 +30,11 @@ const TaskSpace = () => {
             </div>
           </div>
           {/* on going  */}
-          <div className="scale-50 hover:scale-100 duration-700 ease-in-out">
+          <div className="scale-75 hover:scale-100 duration-700 ease-in-out">
             <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mb-6">
               On GOing
             </h2>
-            <div className="border-2 border-gray-700 rounded-t-xl h-[390px] lg:h-[800px] overflow-y-scroll ">
+            <div className="border-2 border-gray-500 h-[800px] overflow-y-scroll ">
               <div>
                 {ongoingTasks?.map((task) => (
                   <TaskSpaceCard2 key={task._id} task={task}></TaskSpaceCard2>
@@ -43,11 +43,11 @@ const TaskSpace = () => {
             </div>
           </div>
           {/* completed  */}
-          <div className="scale-50 hover:scale-100 duration-700 ease-in-out">
+          <div className="scale-75 hover:scale-100 duration-700 ease-in-out">
             <h2 className="font-bold text-3xl text-gray-500 text-center uppercase mb-6">
               Completed
             </h2>
-            <div className="border-2 border-gray-700 rounded-t-xl h-[390px] lg:h-[800px] overflow-y-scroll ">
+            <div className="border-2 border-gray-500 h-[800px] overflow-y-scroll ">
               <div>
                 {completedTasks?.map((task) => (
                   <TaskSpaceCard3 key={task._id} task={task}></TaskSpaceCard3>
