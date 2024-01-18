7aac4f8d00328a661c255dbc574491ef9ab75d9a
Md Noble <mdnoble.wd@gmail.com>
2024-01-18 20:47:17 +0600

    updated dashboard landing media queries

--- src/pages/dashboard/DashboardLanding/DashboardLanding.jsx
+++ src/pages/dashboard/DashboardLanding/DashboardLanding.jsx
@@ -4,7 +4,7 @@ const DashboardLanding = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="text-center">
-        <h2 className="font-bold text-xl md:text-3xl text-[#13ab94] my-12 mx-2">
+        <h2 className="font-bold text-xl lg:text-2xl text-[#13ab94] my-12 mx-2">
           Welcome To Task Pilot Dashboard 🚀
         </h2>
         {/* <p className="font-semibold text-lg text-gray-500 text-center mb-6">
@@ -12,7 +12,7 @@ const DashboardLanding = () => {
           task management a breeze, helping you and your team stay organized,
           collaborate seamlessly and achieve your goals more efficiently.
         </p> */}
-        <img className="w-11/12 lg:w-full mx-auto my-12" src={image} alt="" />
+        <img className="w-10/12 md:w-1/2 lg:w-full mx-auto my-12" src={image} alt="" />
       </div>
     </div>
   );
